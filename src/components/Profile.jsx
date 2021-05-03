49c200d5336f31f387c4cdd933b1c854cf1897eb
Sergey <sergeynechiporenko91@gmail.com>
2021-05-03 22:42:57 +0300

    Added general CSS-modules

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import classes from "./Profile.module.css";
 
 const Profile = () => {
   return (
-    <div className="content">
+    <div className={classes.content}>
         <div>
           <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" />
         </div>
@@ -12,9 +13,9 @@ const Profile = () => {
         <div>
           my posts
           <div>new post</div>
-          <div>
-            <div>post 1</div>
-            <div>post 2</div>
+          <div className={classes.posts}>
+            <div className={classes.item}>post 1</div>
+            <div className={classes.item}>post 2</div>
           </div>
         </div>
       </div>
