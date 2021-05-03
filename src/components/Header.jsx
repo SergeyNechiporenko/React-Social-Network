279564c3b9fcf5df39f6cc4e4f2c7f0058f93f2e
Sergey <sergeynechiporenko91@gmail.com>
2021-05-03 18:04:05 +0300

    Divided into main components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="../public/logo-social.png" />
+    </header>
+  );
+};
+
+export default Header;
