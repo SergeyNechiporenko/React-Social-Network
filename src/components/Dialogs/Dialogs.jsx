c3ae2bdd8c5894c386c9cc6a78358932a06f947c
Sergey <sergeynechiporenko91@gmail.com>
2021-05-10 16:08:10 +0300

    Added Route and NavLink

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import classes from "./Dialogs.module.css";
+
+const Dialogs = () => {
+  return (
+    <div className={classes.dialogs}>
+      <div className={classes.dialogsItems}>
+        <div className={classes.dialog + ' ' + classes.active}>Dimych</div>
+        <div className={classes.dialog}>Andrey</div>
+        <div className={classes.dialog}>Sveta</div>
+        <div className={classes.dialog}>Sasha</div>
+        <div className={classes.dialog}>Victor</div>
+        <div className={classes.dialog}>Valera</div>
+      </div>
+      <div className={classes.messages}>
+        <div className={classes.message}>Hi</div>
+        <div className={classes.message}>How are you?</div>
+        <div className={classes.message}>Yo</div>
+      </div>
+    </div>
+  );
+};
+
+export default Dialogs;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,16 +4,14 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
   return (
-    <div className={classes.content}>
-        <div>
-          <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" />
-        </div>
-        <div>
-          ava+discription
-        </div>
-        <MyPosts />
+    <div>
+      <div>
+        <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" />
       </div>
-  )
-}
+      <div>ava+discription</div>
+      <MyPosts />
+    </div>
+  );
+};
 
 export default Profile;
